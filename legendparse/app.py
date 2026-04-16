67a60542dd89a68203fe31c3f766a62f9149f8ca
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2026-04-15 21:35:02 -0500

    added last updated column, updated table on index render to display, and on update calls datetime now for current time update and writes

--- legendparse/app.py
+++ legendparse/app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime
 app = Flask(__name__)
-
 
 #database items
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 #orm class
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(300), unique=False, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    last_updated = db.Column(db.DateTime, default=datetime.now())
 
     def __repr__(self):
-        return f"Message: {self.message}, Date Created: {self.date_created}"
+        return f"Message: {self.message}, Date Created: {self.date_created}, Last Updated: {self.last_updated}"
     
 #render functions
 @app.route('/')
@@ -51,15 +54,15 @@ def delete_message(id):
 @app.route('/update/<int:id>')
 def message_update(id):
     message_id = Message.query.get_or_404(id)
-    message = message_id.message
     return render_template('update.html', message=message_id.message, id=message_id.id)
 
 @app.route('/update/<int:id>', methods=["POST"])
 def update_message(id):
+    update_time = datetime.now()
     new_message = request.form.get("message")
     
     if new_message is not None:
-        db.session.bulk_update_mappings(Message, [{'id': id, 'message': new_message}])
+        db.session.bulk_update_mappings(Message, [{'id': id, 'message': new_message, 'last_updated': update_time}])
         db.session.commit()
         return redirect('/')
     else:
